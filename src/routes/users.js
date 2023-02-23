ca39b384173426893e20a2353f0f45de2aa6c157
KushTheDoosh <47945636+KushTheDoosh@users.noreply.github.com>
2023-02-23 01:46:07 -0600

    1 file modified

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const  weatherControllerr = require('../weatherController/weatherControllers.js'); 
 
-/* GET users listing. */
+/* GET weather listing. */
 router.get('/', function(req, res, next) {
   weatherControllerr.weather_list(req, res); 
 });
